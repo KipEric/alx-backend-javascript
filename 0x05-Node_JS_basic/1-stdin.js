e71509c06e50cc9308624b965fd869f69b0fba8c
KipEric <kipkoecheric97@gmail.com>
2023-09-26 17:34:08 +0300

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
+  const input = data.toString().trim();
+  process.stdout.write(`Your name is: ${input}\n`);
   if (process.stdin.isTTY) {
     process.exit();
   }
